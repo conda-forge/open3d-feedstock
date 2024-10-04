0c47ae667c4e0a2c9f93dc6ac6d375efc2b832bb
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2024-10-03 22:17:18 -0700

    Disable building shader for linux cross compilation for now

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 set -euxo pipefail
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" && "${target_platform}" == osx-* ]]; then
   (
     mkdir -p build-host
     pushd build-host
@@ -29,7 +29,7 @@ rm -rf build || true
 mkdir build
 cd build
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" && "${target_platform}" == osx-* ]]; then
   export CMAKE_ARGS="${CMAKE_ARGS} -DSHADER_ENCODER_PATH:STRING=`pwd`/../build-host/bin/ShaderEncoder"
   export CMAKE_ARGS="${CMAKE_ARGS} -DSHADER_LINKER_PATH:STRING=`pwd`/../build-host/bin/ShaderLinker"
   export QT_HOST_PATH="$BUILD_PREFIX"
