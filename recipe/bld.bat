0450ee1172930e307ad062f3f25e504c69c80747
Jeongseok Lee <jeongseok@meta.com>
2024-11-13 17:21:28 -0800

    Exit early on failure in each step

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -54,9 +54,13 @@ cmake %SRC_DIR% ^
     -DWITH_FAISS=OFF ^
     -DWITH_IPP=OFF ^
     -DPython3_EXECUTABLE=%PYTHON%
+if errorlevel 1 exit 1
 
 cmake --build . --config Release -- /m:%CPU_COUNT%
+if errorlevel 1 exit 1
 
 cmake --build . --config Release --target install
+if errorlevel 1 exit 1
 
 cmake --build . --config Release --target install-pip-package
+if errorlevel 1 exit 1
