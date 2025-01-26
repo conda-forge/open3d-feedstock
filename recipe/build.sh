cd7f36335931c3d0cbe39e51214089c0f2eb402b
Tobias Fischer <info@tobiasfischer.info>
2025-01-26 13:11:15 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -31,8 +31,8 @@ mkdir build
 cd build
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
-  export CMAKE_ARGS="${CMAKE_ARGS} -DSHADER_ENCODER_PATH:STRING=`pwd`/../build-host/bin/ShaderEncoder"
-  export CMAKE_ARGS="${CMAKE_ARGS} -DSHADER_LINKER_PATH:STRING=`pwd`/../build-host/bin/ShaderLinker"
+  export CMAKE_ARGS="${CMAKE_ARGS} -DSHADER_ENCODER_PATH:STRING=$BUILD_PREFIX/bin/ShaderEncoder"
+  export CMAKE_ARGS="${CMAKE_ARGS} -DSHADER_LINKER_PATH:STRING=$BUILD_PREFIX/bin/ShaderLinker"
   export QT_HOST_PATH="$BUILD_PREFIX"
 else
   export QT_HOST_PATH="$PREFIX"
